0d1f4100fdd2306c13097f6b93d7575a891a48c4
erebuseternal <marcelsanders96@gmail.com>
2024-09-12 03:00:24 +0000

    lambda is working

--- apps/log_odds/tfrecords/lambda/function.py
+++ apps/log_odds/tfrecords/lambda/function.py
@@ -1,25 +1,40 @@
 import os
+import json
+from copy import copy 
+
 import boto3
 
 def handler(event, context):
     client = boto3.client('batch', 'us-east-1')
 
-    job_name = "test-lambda-job"
     job_queue = "mimic-log-odds-job-queue"
     job_definition = "mimic-log-odds-build-tfrecords"
+    
+    dataset = event['dataset']
+    
+    base_config = copy(event)
+    del base_config['train_partitions']
+    del base_config['test_partitions']
+    for train in [True, False]:
+        total_partitions = event['train_partitions'] if train else event['test_partitions']
+        for partition in range(total_partitions):
+            config = copy(base_config)
+            config['train'] = train
+            config['partition'] = partition
+            config['total_partitions'] = total_partitions
 
-    print(f"Submitting job named '{job_name}' to queue '{job_queue}' "
-                f"with definition '{job_definition}'")
-
-    response = client.submit_job(
-        jobName=job_name,
-        jobQueue=job_queue,
-        jobDefinition=job_definition,
-    )
-
-    print(response)
+            command = json.dumps(config).replace(' ', '')
+            client.submit_job(
+                jobName=f"{job_definition}-{dataset}-{train}-{partition}",
+                jobQueue=job_queue,
+                jobDefinition=job_definition,
+                containerOverrides={
+                    "command": [
+                        command
+                    ]
+                }
+            )
 
     return {
-        'statusCode': 200,
-        'body': 'Hello, World!'
+        'statusCode': 200
     }
