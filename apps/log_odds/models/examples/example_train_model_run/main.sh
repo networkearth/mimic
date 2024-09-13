e674c80aec2a817dda5117d62c0c004ffb7078e6
erebuseternal <marcelsanders96@gmail.com>
2024-09-13 11:09:29 +0000

    wip

--- apps/log_odds/models/examples/example_train_model_run/main.sh
+++ apps/log_odds/models/examples/example_train_model_run/main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mimic log-odds run-train-model test-experiment 7459dd13c8e69948bcf78d51b7b7bf1516c0a2f4a65a6eee000e13335732f76c
+mimic log-odds run-train-model test-experiment 4272ee5cdcc0cef84ae3c68bf6416c01c400d4dae67100c8c8adaeddb597ccf3
